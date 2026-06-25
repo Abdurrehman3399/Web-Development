156f15c3db3898c238660232408da10098bb7d8b
[Abdurrehman3399] <[abdurrahmanjaved2@gmail.com].>
2026-06-25 18:27:10 +0500

    First step to words Java Script

---
+++ Complete_Javascript/first.js
@@ -0,0 +1,21 @@
+// let x = BigInt("123")
+// {
+// name = "carry";
+// console.log(name);
+// const a = 100 ;
+// console.log(a);
+// }
+// {
+//  name = "jerry"
+//  console.log (name);
+//  a = 190;
+//  console.log(a)
+// }   
+// let x = BigInt("123");
+// let y = Symbol("helloo!")
+let student = {
+    name :"Admin",
+    age:  "16",
+    price : 199.900,
+    ipass : true,
+}
